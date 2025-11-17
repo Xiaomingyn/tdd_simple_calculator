62b580a2bfa5112b9e428679fa4f5989a4939aab
Xiaomingyn <xiaomingyn@hotmail.com>
2025-11-17 13:58:15 +0100

    Implement functions and their tests.

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,1 +1,41 @@
-# -*- coding: utf-8 -*-
+import operator, bisect
+from functools import reduce
+class SimpleCalculator:
+
+    def add(self, *args):
+        return sum(args)
+
+    def sub(self, a, b):
+        return a - b
+    
+    def mul(self, *args):
+        if not all(args):
+            raise ValueError
+        
+        return reduce(operator.mul, args)
+    
+    def div(self, a, b):
+        try:
+            return a/b
+        except ZeroDivisionError:
+            return float('inf')
+        
+    def avg(self, A, lt = None, ut = None):
+        if len(A) == 0:
+            return 0
+        else: 
+            A_sorted = sorted(A)
+            if not ut:
+                ut = max(A_sorted)
+            if not lt:
+                lt = min(A_sorted)
+                      
+            idut = bisect.bisect(A_sorted, ut)
+            idlt = bisect.bisect(A_sorted, lt)
+            if A_sorted[idlt-1] == lt:
+                idlt = idlt - 1
+             
+            if len(A_sorted[idlt:idut]) == 0:
+                return 0
+            else:  
+                return sum(A_sorted[idlt:idut]) / len(A_sorted[idlt:idut])
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,100 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from simple_calculator.main import SimpleCalculator
+import pytest
 
+def test_add_two_numbers():
+    numbers = range(100)
+    calculator = SimpleCalculator()
+
+    result = calculator.add(*numbers)
+    assert result==4950
+
+def test_subtract_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.sub(10,3)
+    assert result == 7
+
+def test_mul_two_numbers():
+    numbers = range(1,10)
+    calculator = SimpleCalculator()
+
+    result = calculator.mul(*numbers)
+    assert result == 362880
+
+def test_div_two_numbers_float():
+    calculator = SimpleCalculator()
+
+    result = calculator.div(13, 2)
+    assert result == 6.5
+
+def test_div_by_zero_returns_inf():
+    calculator = SimpleCalculator()
+
+    result = calculator.div(5, 0)
+    assert result == float('inf')
+
+def test_mul_by_zero_raises_exception():
+    calculator = SimpleCalculator()
+
+    with pytest.raises(ValueError):
+        calculator.mul(3, 0)        
+
+def test_avg_of_iterable():
+    numbers = range(10)
+    calculator = SimpleCalculator()
+
+    result = calculator.avg(numbers)
+    assert result == 4.5
+
+def test_avg_by_type_raises_exception():
+    # varified by the "sum" function already, but it worths to keep the test here.
+    calculator = SimpleCalculator()
+
+    with pytest.raises(TypeError):
+        calculator.avg(5)
+
+def test_avg_by_upper_threshold():
+    numbers = range(10)
+    calculator = SimpleCalculator()
+
+    result = calculator.avg(numbers, ut=8)
+    assert result == 4.0
+
+def test_avg_by_lower_threshold():
+    numbers = range(10)
+    calculator = SimpleCalculator()
+
+    result = calculator.avg(numbers, lt=2)
+    assert result == 5.5
+
+def test_avg_upper_threshold_is_included():
+    calculator = SimpleCalculator()
+
+    result = calculator.avg([2, 5, 12, 98], ut=98)
+    assert result == 29.25
+
+def test_avg_lower_threshold_is_included():
+    calculator = SimpleCalculator()
+
+    result = calculator.avg([2, 5, 12, 98], lt=2)
+    assert result == 29.25    
+
+def test_avg_by_empty_iterable():
+    numbers = []
+    calculator = SimpleCalculator()
+
+    result = calculator.avg(numbers)
+    assert result == 0
+
+def test_avg_by_empty_after_outlier_removal():
+    numbers = [2, 5, 7, 11, 45, 65, 79]
+    calculator = SimpleCalculator()
+
+    result = calculator.avg(numbers, lt=15, ut=40)
+    assert result == 0    
+
+def test_avg_manages_empty_list_before_outlier_removal():
+    calculator = SimpleCalculator()
+
+    result = calculator.avg([], lt=15, ut=90)
+    assert result == 0    
